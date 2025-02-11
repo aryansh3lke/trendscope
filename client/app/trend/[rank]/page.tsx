5045bb4060fb4958fc6f59872af89888c5692114
AryanShelke <aryan.shelke.2003@gmail.com>
2025-02-10 22:43:07 -0800

    Add useEffect hook to avoid accessing null data
    - Add useEffect hook to set the trend for the trend page after the trendData has loaded and display loading if it hasn't yet

--- client/app/trend/[rank]/page.tsx
+++ client/app/trend/[rank]/page.tsx
@@ -5,23 +5,32 @@ import { useParams } from "next/navigation";
 import VantaGlobe from "@/components/VantaGlobe";
 import TrendPanel from "@/components/TrendPanel";
 import TweetCarousel from "@/components/TweetCarousel";
+import { useEffect, useState } from "react";
 
 export default function TrendPage() {
   const { rank } = useParams<{ rank: string }>();
   const trendsData: TrendsData | null = useTrendsData();
-  const trend: Trend = trendsData!.data[Number(rank)];
+  const [trend, setTrend] = useState<Trend | null>(null);
+
+  useEffect(() => {
+    if (trendsData) setTrend(trendsData.data[Number(rank)] ?? null);
+  }, [trendsData]);
 
   return (
     <main className="min-h-screen p-5 max-sm:max-w-lg">
       <VantaGlobe />
-      <div className="flex flex-col lg:flex-row items-center justify-center gap-4">
-        <div className="lg:w-1/2 h-[500px] max-[400px]:max-w-72 max-[480px]:max-w-80 max-sm:max-w-md max-lg:max-w-xl">
-          {trend && <TrendPanel trend={trend} />}
+      {trendsData ? (
+        <div className="flex flex-col lg:flex-row items-center justify-center gap-4">
+          <div className="lg:w-1/2 h-[500px] max-[400px]:max-w-72 max-[480px]:max-w-80 max-sm:max-w-md max-lg:max-w-xl">
+            {trend && <TrendPanel trend={trend} />}
+          </div>
+          <div className="lg:w-1/2 h-[500px] max-[400px]:max-w-72 max-[480px]:max-w-80 max-sm:max-w-md max-lg:max-w-xl">
+            {trend && <TweetCarousel tweets={trend.tweets} />}
+          </div>
         </div>
-        <div className="lg:w-1/2 h-[500px] max-[400px]:max-w-72 max-[480px]:max-w-80 max-sm:max-w-md max-lg:max-w-xl">
-          {trend && <TweetCarousel tweets={trend.tweets} />}
-        </div>
-      </div>
+      ) : (
+        <h1 className="font-medium text-5xl text-center mb-4">Loading...</h1>
+      )}
     </main>
   );
 }

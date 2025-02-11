0d3f5bff2f459825fc934e9cb3664296e46200b5
AryanShelke <aryan.shelke.2003@gmail.com>
2025-02-11 02:20:55 -0800

    Refresh trend data at a faster rate

--- client/lib/data.ts
+++ client/lib/data.ts
@@ -3,7 +3,7 @@ import { NEXT_PUBLIC_BACKEND_PROXY } from "@/lib/proxy";
 export async function getTrendsData() {
   console.log(NEXT_PUBLIC_BACKEND_PROXY);
   const response = await fetch(NEXT_PUBLIC_BACKEND_PROXY + "/api/fetch-data", {
-    next: { revalidate: 1800 }, // Refresh data every 30 minutes
+    next: { revalidate: 600 }, // Refresh data every 10 minutes
   });
 
   if (!response.ok) throw new Error("Failed to fetch trends");
